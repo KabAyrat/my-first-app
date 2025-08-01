451eb03abd32826a32a3ca3b1e0b5221eb091577
KabAyrat <kabirov.ayrat2004@gmail.com>
2025-08-01 18:02:42 +0300

    фикс бага с неотображением без начальных данных

---
+++ src/data/initialTodos.js
@@ -0,0 +1,10 @@
+// src/data/initialTodos.js
+export const initialTodos = [
+    {
+        id: '1',
+        text: 'Пример задачи(без него не работает нихуя)',
+        completed: false,
+        priority: 'high',
+        createdAt: new Date('2024-01-15').toISOString(),
+    }
+];
